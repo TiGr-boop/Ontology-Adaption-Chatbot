ee546412fdc5b1936016fa5442e1ef495979208a
TiGr-boop <tim1999.grosch@gmail.com>
2026-08-03 14:49:30 +0200

    Fixed small logging error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.create_collection import create_collection_from_ontology
 from src.start_chatbot import start_chainlit
 import logging
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger(__name__)
 
 def main():
     create_collection_from_ontology()
